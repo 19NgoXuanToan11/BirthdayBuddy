994c688c648c746bf4f7eb6db611e88a354fcd18
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-01-09 11:14:04 +0700

    update

---
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/atoms/guest-header/header.tsx
@@ -0,0 +1,13 @@
+import './header.scss'
+
+function GuestHeader() {
+  return (
+    <>
+    <div className="guest-header-container">
+        <h1>This is guest header</h1>
+    </div>
+    </>
+  )
+}
+
+export default GuestHeader
