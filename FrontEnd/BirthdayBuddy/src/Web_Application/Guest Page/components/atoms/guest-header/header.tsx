768f09504599da67ddb8c8360a469fcf1d1538f6
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-01-10 11:30:45 +0700

    update guest FE

--- FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/atoms/guest-header/header.tsx
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/atoms/guest-header/header.tsx
@@ -4,23 +4,31 @@ function GuestHeader() {
   return (
     <>
       <div className="guest-header-container">
-        <div className="upper-header">
-          <p className='shipping'>Giao hàng</p>
-          <p className='location'>Thành phố Hồ Chí Minh</p>
-          <p><a href="">Theo dõi đơn hàng</a>/<a href="">Trợ giúp</a></p>
-        </div>
-        <div className="middle-header">
-          <ul>
-            <li>Trang chủ</li>
-            <li>Địa điểm</li>
-            <li>Sản phẩm</li>
-            <li>Chủ đề bữa tiệc</li>
-            <li>Về BirthdayBuddy</li>
-          </ul>
-        </div>
-        <div className="lower-header">
-          lower
-        </div>
+        <nav>
+          <div className="upper-header">
+            <p className='shipping'><a href="">Giao hàng</a> | <a href="">Thành phố Hồ Chí Minh</a></p>
+            <p className='tracking'><a href="">Theo dõi đơn hàng</a>/<a href="">Trợ giúp</a></p>
+          </div>
+          <div className="middle-header">
+            <img src="../../../../../SWP_RESOURCE/Logo.png" alt="center-logo.png" />
+            <div className="search-bar">
+              <input type="text" placeholder="Tìm kiếm..." />
+              <button><i className="fas fa-search"></i></button>
+            </div>
+            <div className="icon-middle-header">
+              
+            </div>
+          </div>
+          <div className="lower-header">
+            <ul>
+              <li>Trang chủ</li>
+              <li>Địa điểm</li>
+              <li>Sản phẩm</li>
+              <li>Chủ đề bữa tiệc</li>
+              <li>Về BirthdayBuddy</li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </>
   )
