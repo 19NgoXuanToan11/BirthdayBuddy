6d738ff90ab2d13154ec4c26ddc062e5a96c1a80
19NgoXuanToan11 <toannxse171297@fpt.edu.vn>
2024-01-24 16:29:48 +0700

    Update smooth logo for header.tsx
    Truck + Plane + Person +  Cart

--- FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/atoms/guest-header/header.tsx
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/atoms/guest-header/header.tsx
@@ -4,10 +4,13 @@ function GuestHeader() {
     return (
         <header className="guest-header">
             <div className="upper-header">
-                <a href="#">Giao hàng</a> | {" "}
-                <a href="#">Thành phố Hồ Chí Minh</a>
-                <div className="tracking">
-                    <a href="#">Theo dõi đơn hàng</a> | <a href="#">Trợ giúp</a>
+                <div className="upper-header-1">
+                    <a href="#"><img src="/src/SWP_RESOURCE/icon/truck.gif"></img>Giao hàng</a><span> |</span> {" "}
+                    <a href="#"><img src="/src/SWP_RESOURCE/icon/airplane.gif"></img>Thành phố Hồ Chí Minh</a>
+                </div>
+                <div className="upper-header-2">
+                    <a href="#">Theo dõi đơn hàng</a><span>|  </span>
+                    <a href="#">Trợ giúp</a>
                 </div>
             </div>
             <div className="middle-header">
@@ -20,10 +23,10 @@ function GuestHeader() {
                 </div>
                 <div className="user-cart">
                     <a href="#">
-                        <i className="fa fa-user"><img src="/src/SWP_RESOURCE/icon/user.png"/></i>
+                        <i className="fa fa-user"><img src="/src/SWP_RESOURCE/icon/alone.gif"/></i>
                     </a>
                     <a href="#">
-                        <i className="fa fa-shopping-cart"><img src="/src/SWP_RESOURCE/icon/shopping-cart.png"/></i>
+                        <i className="fa fa-shopping-cart"><img src="/src/SWP_RESOURCE/icon/shopping-cart.gif"/></i>
                     </a>
                 </div>
             </div>
