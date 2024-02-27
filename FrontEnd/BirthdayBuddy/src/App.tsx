2678f354ed318a9ae5d479cb6afe9ba56545b905
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-02-27 22:13:31 +0700

    update page flow in Guest

--- FrontEnd/BirthdayBuddy/src/App.tsx
+++ FrontEnd/BirthdayBuddy/src/App.tsx
@@ -3,13 +3,21 @@ import "./App.scss";
 import GuestHomePage from "./Web_Application/Guest Page/components/pages/home-page/guest-home-page";
 import LoginPage from "./Web_Application/Authorization Page/components/pages/login-page";
 import PartyCheckout from "./Web_Application/Guest Page/components/molecules/party-checkout/party-checkout";
+import RegisterPage from "./Web_Application/Authorization Page/components/pages/register-page";
+import ForgetPassword from "./Web_Application/Authorization Page/components/molecules/forget-password/forgetpw";
+import GuestRestaurantListPage from "./Web_Application/Guest Page/components/pages/restaurant-list-page/guest-restaurant-list-page";
+import GuestRestaurantDetailsPage from "./Web_Application/Guest Page/components/pages/restaurant-details-page/restaurant-details-page";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<GuestHomePage />} />
+                <Route path='/restaurant-list' element={<GuestRestaurantListPage />} />
+                <Route path='/restaurant-details/:id' element={<GuestRestaurantDetailsPage />} />
                 <Route path='/login' element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/forgetpw" element={<ForgetPassword />} />
                 <Route path='/check-out' element={<PartyCheckout />} />
             </Routes>
         </Router>
---
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/pages/restaurant-list-page/guest-restaurant-list-page.tsx
@@ -0,0 +1,22 @@
+import GuestRestaurantListHeader from "../../atoms/guest-restaurant-list-header/guest-restaurant-list-header"
+import GuestFooterHome from "../../organisms/guest-footer-home/guest-footer-home"
+import RestaurantListTemplate from "../../templates/restaurant-list-template/restaurant-list-template"
+
+function GuestRestaurantListPage() {
+    return (
+        <>
+            <header>
+                <GuestRestaurantListHeader />
+            </header>
+            <div className="body-container">
+                <RestaurantListTemplate />
+            </div>
+            <footer>
+                <GuestFooterHome />
+            </footer>
+        </>
+
+    )
+}
+
+export default GuestRestaurantListPage
