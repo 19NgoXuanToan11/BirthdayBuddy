4cc0ba28470f83218471d553a5dc29332651f3f3
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-02-28 11:53:29 +0700

    update page flow

--- FrontEnd/BirthdayBuddy/src/App.tsx
+++ FrontEnd/BirthdayBuddy/src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
+//import Guest Pages
 import GuestHomePage from "./Web_Application/Guest Page/components/pages/home-page/guest-home-page";
-import LoginPage from "./Web_Application/Authorization Page/components/pages/login-page";
-import PartyCheckout from "./Web_Application/Guest Page/components/molecules/party-checkout/party-checkout";
-import RegisterPage from "./Web_Application/Authorization Page/components/pages/register-page";
-import ForgetPassword from "./Web_Application/Authorization Page/components/molecules/forget-password/forgetpw";
 import GuestRestaurantListPage from "./Web_Application/Guest Page/components/pages/restaurant-list-page/guest-restaurant-list-page";
 import GuestRestaurantDetailsPage from "./Web_Application/Guest Page/components/pages/restaurant-details-page/restaurant-details-page";
+
+//import Authorized Pages
+import LoginPage from "./Web_Application/Authorization Page/components/pages/login-page";
+import RegisterPage from "./Web_Application/Authorization Page/components/pages/register-page";
+import ForgetPassword from "./Web_Application/Authorization Page/components/molecules/forget-password/forgetpw";
+
+//import Customer Pages
 import CustomerHome from "./Web_Application/Customer Page/page/home-page/customer-home-page";
 import CustomerRestaurantListPage from "./Web_Application/Customer Page/page/restaurant-page/customer-restaurant-page";
 import CustomerRestaurantDetailsPage from "./Web_Application/Customer Page/page/customer-restaurant-details-page/customer-restaurant-details-page";
+import CustomerCheckoutPage from "./Web_Application/Customer Page/page/checkout-page/customer-checkout-page";
+
+//import Party Hosts Pages 
+//import Administator Pages
 
 function App() {
 
@@ -22,10 +30,10 @@ function App() {
                 <Route path='/login' element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/forgetpw" element={<ForgetPassword />} />
-                <Route path='/check-out' element={<PartyCheckout />} />
                 <Route path='/customer' element={<CustomerHome />} />
                 <Route path='/customer/restaurant-list' element={<CustomerRestaurantListPage />} />
                 <Route path='/customer/restaurant-details/:id' element={<CustomerRestaurantDetailsPage />} />
+                <Route path='/customer/check-out' element={<CustomerCheckoutPage />} />
             </Routes>
         </Router>
     );
