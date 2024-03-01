82124323dc48b1924cf8fe737fbcaec000596831
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-03-02 00:56:41 +0700

    update page flow (almost done)

--- FrontEnd/BirthdayBuddy/src/Web_Application/Customer Page/page/momo-payment-page/momo-payment-page.tsx
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Customer Page/page/momo-payment-page/momo-payment-page.tsx
@@ -1,19 +1,22 @@
 import CustomerHeaderHome from '../../organisms/customer-header-home/customer-header-home'
 import MomoPaymentTemplate from '../../template/scan-payment-template/momo-payment-template'
 import CustomerFooterHome from '../../organisms/customer-footer-home/customer-footer-home'
+import { Link } from 'react-router-dom'
 
 function MomoPaymentPage() {
   return (
     <>
-    <header>
+      <header>
         <CustomerHeaderHome />
-    </header>
-    <div className="body">
-        <MomoPaymentTemplate />
-    </div>
-    <footer>
+      </header>
+      <Link to="/customer/booking-success">
+        <div className="body">
+          <MomoPaymentTemplate />
+        </div>
+      </Link>
+      <footer>
         <CustomerFooterHome />
-    </footer>
+      </footer>
     </>
   )
 }
