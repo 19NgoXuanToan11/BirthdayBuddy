72510489774b7256781b63104c6e52810fea5eb2
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-03-02 12:12:33 +0700

    update new header

---
+++ FrontEnd/BirthdayBuddy/src/Web_Application/Guest Page/components/organisms/guest-header-restaurantlist/guest-header-restaurantlist.tsx
@@ -0,0 +1,11 @@
+import GuestHeaderRestaurantList from '../../atoms/guest-header-restaurantlist/guest-header-restaurantlist'
+
+function GuestHeaderRestaurantListTemplate() {
+  return (
+    <>
+    <GuestHeaderRestaurantList />
+    </>
+  )
+}
+
+export default GuestHeaderRestaurantListTemplate
