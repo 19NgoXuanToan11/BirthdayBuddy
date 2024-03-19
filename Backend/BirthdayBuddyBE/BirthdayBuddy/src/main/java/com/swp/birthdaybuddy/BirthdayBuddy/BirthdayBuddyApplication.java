e61d98f1e2d0d947be49c78bd244dea02f32430a
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-03-19 14:12:05 +0700

    Update BirthdayBuddyApplication.java

--- Backend/BirthdayBuddyBE/BirthdayBuddy/src/main/java/com/swp/birthdaybuddy/BirthdayBuddy/BirthdayBuddyApplication.java
+++ Backend/BirthdayBuddyBE/BirthdayBuddy/src/main/java/com/swp/birthdaybuddy/BirthdayBuddy/BirthdayBuddyApplication.java
@@ -3,10 +3,7 @@ package com.swp.birthdaybuddy.BirthdayBuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class BirthdayBuddyApplication {
 	public static void main(String[] args) {
